756e9c7e26407e4262c7b680fdbd6e6a29def6b8
shiori771 <16hb218l@gmail.com>
2020-10-16 17:56:58 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: '※英字と数字の両方を含めて設定してください' 
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/} do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/} do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+  validates :birth_date, presence: true
+  
 end
