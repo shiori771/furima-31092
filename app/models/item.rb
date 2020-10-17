f4a6faff2e5ade9b27425ad33f3ff5a981fd1a13
shiori771 <16hb218l@gmail.com>
2020-10-18 08:15:26 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category, :status, :shipping_cost, :shipping_prefecture, :shipping_date
+
+  validates :name, :text, :category, :status, :shipping_cost, :shipping_prefecture, :shipping_date, :price, presence: true
+
+  validates :category_id, :status_id, :shipping_cost_id, :shipping_prefecture_id, :shipping_date_id, numericality: { other_than: 1 }
 end
