18d04bc9935a4c8d95822d95b2a81ca2a0ce454f
shiori771 <16hb218l@gmail.com>
2020-10-18 08:08:45 +0900

    active hashのアソシエーション作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :status, :shipping_cost, :shipping_prefecture, :shipping_date
 end
