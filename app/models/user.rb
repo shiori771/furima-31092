3e4e681a08fb9ce5ba28bc1898f2f5b610ca8dc0
shiori771 <16hb218l@gmail.com>
2020-10-17 11:13:12 +0900

    半角全角エラーメッセージ表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User < ApplicationRecord
   validates :nickname, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: '※英字と数字の両方を含めて設定してください' 
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/} do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/,  message: 'Full-width characters'} do
     validates :first_name
     validates :last_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/} do
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width characters'} do
     validates :first_name_kana
     validates :last_name_kana
   end
