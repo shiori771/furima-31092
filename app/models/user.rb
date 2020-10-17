f13690c92493d66b64a423d4ee1a7d060c4b2042
shiori771 <16hb218l@gmail.com>
2020-10-17 14:31:03 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,18 +7,17 @@ class User < ApplicationRecord
   validates :nickname, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/,  message: 'Full-width characters'} do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
     validates :first_name
     validates :last_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width characters'} do
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width characters' } do
     validates :first_name_kana
     validates :last_name_kana
   end
 
   validates :birth_date, presence: true
-  
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname { "yamada" }
+    nickname { 'yamada' }
     email { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
     password { password }
     password_confirmation { password }
-    first_name { "ぜんかく" }
-    last_name { "ぜんかく" }
-    first_name_kana { "ゼンカクカナ" }
-    last_name_kana { "ゼンカクカナ" }
-    birth_date { "2020-01-01" }
+    first_name { 'ぜんかく' }
+    last_name { 'ぜんかく' }
+    first_name_kana { 'ゼンカクカナ' }
+    last_name_kana { 'ゼンカクカナ' }
+    birth_date { '2020-01-01' }
   end
 end
-
-
