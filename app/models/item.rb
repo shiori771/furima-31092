2abd5a75798b1f1ababbf1e3eef36e9b60329af9
shiori771 <16hb218l@gmail.com>
2020-10-18 17:39:25 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,26 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :text, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999,  only_integer: true }, format: { with: /\A[0-9]+\z/ }
+  
+  validates :category_id,  presence: true
+  validates :status_id, presence: true
+  validates :shipping_cost_id, presence: true
+  validates :shipping_prefecture_id, presence: true
+  validates :shipping_date_id, presence: true
+  
+  
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :shipping_cost_id, numericality: { other_than: 1 }
+  validates :shipping_prefecture_id, numericality: { other_than: 1 }
+  validates :shipping_date_id, numericality: { other_than: 1 }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category, :status, :shipping_cost, :shipping_prefecture, :shipping_date
-
-  validates :name, :text, :category, :status, :shipping_cost, :shipping_prefecture, :shipping_date, :price, presence: true
-
-  validates :category_id, :status_id, :shipping_cost_id, :shipping_prefecture_id, :shipping_date_id, numericality: { other_than: 1 }
-
-  VALID_PRICE_REGEX = /\A[0-9]+\z/
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true }, format: { with: VALID_PRICE_REGEX }
+  
 end
