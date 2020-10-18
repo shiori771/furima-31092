b6b1db4ce18ad02581713ef293f8ba93ee16160d
shiori771 <16hb218l@gmail.com>
2020-10-18 11:49:34 +0900

    販売価格のバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,6 @@ class Item < ApplicationRecord
   validates :name, :text, :category, :status, :shipping_cost, :shipping_prefecture, :shipping_date, :price, presence: true
 
   validates :category_id, :status_id, :shipping_cost_id, :shipping_prefecture_id, :shipping_date_id, numericality: { other_than: 1 }
+
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true } 
 end
