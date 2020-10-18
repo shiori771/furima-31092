548c5f82aac87fcded107f1509a79ac77d8c3698
shiori771 <16hb218l@gmail.com>
2020-10-18 11:36:57 +0900

    出品画面の表示と保存

---
+++ app/javascript/math.js
@@ -0,0 +1,16 @@
+function math () {
+  const Tax = 0.1;
+  const Price = document.getElementById("item-price");
+  Price.addEventListener("keyup", () => {
+    const ItemPrice = Price.value
+    const TaxPrice = document.getElementById("add-tax-price");
+    const Profit = document.getElementById("profit");
+    const TaxFee = ItemPrice * Tax;
+    const afterTaxFee = Math.floor(TaxFee);
+    const ProfitFee = ItemPrice - afterTaxFee;
+    TaxPrice.innerHTML = afterTaxFee;
+    Profit.innerHTML = ProfitFee;
+  });
+}
+
+window.addEventListener("load", math);
