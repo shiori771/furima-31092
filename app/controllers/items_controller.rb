6cf70988b07b5dc6c443051db4688e8c8512be7d
shiori771 <16hb218l@gmail.com>
2020-10-19 18:27:48 +0900

    edit, createアクションでルーティング・コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -10,7 +11,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def create
@@ -22,9 +22,20 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def create
+    
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :status_id, :shipping_cost_id, :shipping_prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
