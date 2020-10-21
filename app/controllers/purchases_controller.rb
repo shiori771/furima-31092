487fa1412c45464f86eaea57e11ffd363a11470e
shiori771 <16hb218l@gmail.com>
2020-10-22 08:11:49 +0900

    [リファクタリング]pay_order作成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,26 +8,31 @@ class PurchasesController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: Item.find(params[:item_id]).price,
-        card: order_params[:token],
-        currency: 'jpy'
-      )
-       @order_address.save
-       redirect_to root_path
+      pay_order
+      @order_address.save
+      return redirect_to root_path
     else
       render :index
     end
   end
-
+  
   private
-
+  
   def order_params
     params.require(:order_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number, :purchase_id).merge(token: params[:token], user_id: current_user.id, item_id: Item.find(params[:item_id]).id)
   end
-
+  
   def set_purchase
     @item = Item.find(params[:item_id])
   end
+  
+  def pay_order
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: Item.find(params[:item_id]).price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+  
 end
