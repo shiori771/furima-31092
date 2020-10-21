27f62a16410376849d534735b1ad8d2dbc0deb12
shiori771 <16hb218l@gmail.com>
2020-10-22 08:28:15 +0900

    購入ページ遷移のユーザー制限

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,6 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :move_to_index
   before_action :set_purchase, only: [:index, :create]
 
   def index
@@ -17,6 +19,12 @@ class PurchasesController < ApplicationController
   end
   
   private
+
+  def move_to_index
+    if current_user.id == Item.find(params[:item_id]).user_id
+      redirect_to root_path
+    end
+  end
   
   def order_params
     params.require(:order_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number, :purchase_id).merge(token: params[:token], user_id: current_user.id, item_id: Item.find(params[:item_id]).id)
