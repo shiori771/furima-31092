dff10fb864bfcc9ca2d20d60de32bacf0576d762
shiori771 <16hb218l@gmail.com>
2020-10-21 12:46:40 +0900

    createアクションの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,6 +5,7 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
        @order_address.save
