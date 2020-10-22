f24354855beaca6947951da1c48d91ca5f3c4673
shiori771 <16hb218l@gmail.com>
2020-10-22 13:53:46 +0900

    rubocop実行

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,7 +3,7 @@ class PurchasesController < ApplicationController
   before_action :move_to_index
   before_action :restrict_sold_out
   before_action :set_purchase
-  
+
   def index
     @order_address = OrderAddress.new
     @order_address.present?
@@ -14,28 +14,24 @@ class PurchasesController < ApplicationController
     if @order_address.valid?
       pay_order
       @order_address.save
-      return redirect_to root_path
+      redirect_to root_path
     else
       render :index
     end
   end
-  
+
   private
-  
+
   def move_to_index
-    if current_user.id == Item.find(params[:item_id]).user_id
-      redirect_to root_path
-    end
+    redirect_to root_path if current_user.id == Item.find(params[:item_id]).user_id
   end
 
   def restrict_sold_out
     set_purchase
     @order_done = Purchase.find_by_item_id(params[:item_id])
-    if @order_done.present?
-        return redirect_to root_path
-    end
+    return redirect_to root_path if @order_done.present?
   end
-  
+
   def set_purchase
     @item = Item.find(params[:item_id])
   end
@@ -43,14 +39,13 @@ class PurchasesController < ApplicationController
   def order_params
     params.require(:order_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number, :purchase_id).merge(token: params[:token], user_id: current_user.id, item_id: Item.find(params[:item_id]).id)
   end
-  
+
   def pay_order
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: Item.find(params[:item_id]).price,
       card: order_params[:token],
       currency: 'jpy'
     )
   end
-  
 end
--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :order_address do
-    token {"tok_abcdefghijk00000000000000000"}
+    token { 'tok_abcdefghijk00000000000000000' }
     postcode { '123-4567' }
     prefecture_id { 3 }
     city { '横浜市緑区' }
     block { '青山4-1' }
     building { '新緑ビル205' }
-    phone_number { 12345678901 } 
+    phone_number { 12_345_678_901 }
   end
 end
